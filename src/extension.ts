aeeca924244008fbf890ce633cac6218c60e1c59
tyler <tylerliu362@gmail.com>
2023-05-23 22:53:40 +0800

    Add changes to treeItem.iconPath and treeItem.description; implement select, deselect, and isSelected methods

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,17 @@ import { FileTreeProvider } from './treeView';
 export function activate(context: vscode.ExtensionContext) {
   const fileTreeProvider = new FileTreeProvider();
   vscode.window.createTreeView('gpt-copytree-panel', { treeDataProvider: fileTreeProvider });
-}
 
+  context.subscriptions.push(
+    vscode.commands.registerCommand('GPT-CopyTree.toggleSelect', (fileItem) => {
+      if (fileTreeProvider.isSelected(fileItem)) {
+        fileTreeProvider.deselect(fileItem);
+      } else {
+        fileTreeProvider.select(fileItem);
+      }
+    }),
+    vscode.commands.registerCommand('GPT-CopyTree.refresh', () => fileTreeProvider.refreshAll())
+  );
+}
 
 export function deactivate() {}
--- src/treeView.ts
+++ src/treeView.ts
@@ -2,36 +2,66 @@ import * as vscode from 'vscode';
 import { getFilesAndDirectories, FileItem } from './fileSystem';
 
 export class FileTreeProvider implements vscode.TreeDataProvider<FileItem> {
-  onDidChangeTreeData?: vscode.Event<FileItem | undefined | null | void>;
+  private _onDidChangeTreeData: vscode.EventEmitter<FileItem | undefined | null | void> = new vscode.EventEmitter<FileItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<FileItem | undefined | null | void> = this._onDidChangeTreeData.event;
+
+  private selectedItems: Set<string> = new Set();
+
+  refresh(item?: FileItem): void {
+    this._onDidChangeTreeData.fire(item);
+  }
+
+  refreshAll(): void {
+    this._onDidChangeTreeData.fire();
+  }
 
   getTreeItem(element: FileItem): vscode.TreeItem {
-    const treeItem = new vscode.TreeItem(element.name);
-    treeItem.collapsibleState = element.isDirectory ?
-      vscode.TreeItemCollapsibleState.Collapsed :
-      vscode.TreeItemCollapsibleState.None;
-    treeItem.contextValue = element.isDirectory ? 'directory' : 'file';
+    const treeItem = new vscode.TreeItem(
+      element.name,
+      element.isDirectory ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
+    );
+
     treeItem.command = {
-      command: 'GPT-CopyTree.select',
-      title: 'Select',
+      command: 'GPT-CopyTree.toggleSelect',
       arguments: [element],
+      title: 'Toggle Select'
     };
+
+    if (element.isDirectory) {
+      treeItem.iconPath = new vscode.ThemeIcon("folder");
+    } else {
+      treeItem.iconPath = new vscode.ThemeIcon("file");
+    }
+
+    if (this.isSelected(element)) {
+      treeItem.description = "✅";
+    } else {
+      treeItem.description = "☑️";
+    }
+
     return treeItem;
   }
 
-  getChildren(element?: FileItem): vscode.ProviderResult<FileItem[]> {
+  getChildren(element?: FileItem): Thenable<FileItem[]> {
     if (element) {
-      return element.children;
+      return Promise.resolve(getFilesAndDirectories(element.path));
     } else {
-      if (vscode.workspace.workspaceFolders) {
-        return getFilesAndDirectories(vscode.workspace.workspaceFolders[0].uri.fsPath || '');
-      } else {
-        const noWorkspaceItem: FileItem = {
-          name: 'No folder or workspace opened',
-          path: '',
-          isDirectory: false
-        };
-        return [noWorkspaceItem];
-      }
+      const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+      return Promise.resolve(workspaceFolder ? getFilesAndDirectories(workspaceFolder.uri.fsPath) : []);
     }
+  }
+
+  select(item: FileItem): void {
+    this.selectedItems.add(item.path);
+    this.refresh(item);
+  }
+
+  deselect(item: FileItem): void {
+    this.selectedItems.delete(item.path);
+    this.refresh(item);
+  }
+
+  isSelected(item: FileItem): boolean {
+    return this.selectedItems.has(item.path);
   }
 }
